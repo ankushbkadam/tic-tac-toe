b62acd2f2fb11d398c6a6469225f7f223e87a5ef
Ankush Kadam <ankushbkadam@gmail.com>
2023-04-02 13:04:49 +0530

    code refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import Game from './TicTacToe';
+import React from "react";
+import "./App.css";
+import Game from "./TicTacToe";
 
 function App() {
-  return (
-    <div className="App">
-     <Game />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Game />
+		</div>
+	);
 }
 
 export default App;
